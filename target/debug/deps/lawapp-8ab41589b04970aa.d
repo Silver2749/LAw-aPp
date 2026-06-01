2ac79fa4025cc21b1ae1d5121eb95c70c7eee9d6
Silver2749 <7shanebraganza@gmail.com>
2026-06-01 05:54:48 +0530

    First commit!

---
+++ target/debug/deps/lawapp-8ab41589b04970aa.d
@@ -0,0 +1,5 @@
+C:\Users\91859\Dev-Shi\rust\lawapp\target\debug\deps\lawapp-8ab41589b04970aa.d: src\main.rs
+
+C:\Users\91859\Dev-Shi\rust\lawapp\target\debug\deps\liblawapp-8ab41589b04970aa.rmeta: src\main.rs
+
+src\main.rs:
---
+++ target/debug/deps/lawapp-bfa99746d4315a13.d
@@ -0,0 +1,5 @@
+C:\Users\91859\Dev-Shi\rust\lawapp\target\debug\deps\lawapp-bfa99746d4315a13.d: src\main.rs
+
+C:\Users\91859\Dev-Shi\rust\lawapp\target\debug\deps\liblawapp-bfa99746d4315a13.rmeta: src\main.rs
+
+src\main.rs:
